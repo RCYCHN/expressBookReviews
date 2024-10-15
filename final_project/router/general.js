06c7422025426077fe899e3efa86f5faf6dd4b31
RCYCHN <rickychun39@gmail.com>
2024-10-15 14:38:52 +0200

    Completed the code for getting the book details based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,14 +12,22 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Respond with the books list in JSON format
+  return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { isbn } = req.params; // Retrieve the ISBN from the request parameters
+  const book = books.find(book => book.isbn === isbn); // Find the book in the array
+
+  if (book) {
+    // If the book is found, send it in the response
+    return res.status(200).send(JSON.stringify(book, null, 4)); // Send the book details
+  } else {
+    // If the book is not found, send a 404 error
+    return res.status(404).json({ message: "Book not found" });
+  }
  });
   
 // Get book details based on author
